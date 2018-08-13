150052794445a6457661878172985dcdcee56353
Johs Kristoffersen <johs.kristoffersen@audienceproject.com>
2018-08-13 12:23:11 +0200

    support overwrite

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
+
 #!/bin/bash
+for var in `echo $WERCKER_ENV_TEMPLATE_OVERWRITE | tr "," "\n"`; do
+    eval "export $var"
+done
+
 perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < $WERCKER_ENV_TEMPLATE_INPUT > $WERCKER_ENV_TEMPLATE_OUTPUT
